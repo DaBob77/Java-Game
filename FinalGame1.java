51f8d4a56f95682c3c9589801df710019ad0c2ad
DaBob77 <162922068+DaBob77@users.noreply.github.com>
2025-04-17 09:09:31 -0500

    CASZ

--- FinalGame1.java
+++ FinalGame1.java
@@ -3,7 +3,8 @@ import java.util.*;
 import java.awt.*;
 public class FinalGame1{
     public static void main(String[] args){
-        
+        JFrame frame = new JFrame("Portal 2d");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
     }
 
 
