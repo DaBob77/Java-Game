f8f5131d7aa1ab0020a42daecc025af9b3fb3fb3
Ethan Petersen <roboblockhead@gmail.com>
2025-04-23 09:31:57 -0500

    ccc

--- FinalGame1.java
+++ FinalGame1.java
@@ -2,12 +2,22 @@ import javax.swing.*;
 import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
 
 public class FinalGame1 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Portal 2d");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1080, 720);
+        try {
+            BufferedImage playerImage = ImageIO.read(new File(""));
+        } catch(IOException e) {
+            System.out.println("Image failed to load");
+            e.printStackTrace();
+        }
+
 
         JPanel gamePanel = new JPanel() {
             @Override
