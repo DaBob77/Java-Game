e7e5fb8646a08468c2629885f71b9fa81b555f69
Ethan Petersen <roboblockhead@gmail.com>
2025-04-23 09:31:19 -0500

    ddd

--- FinalGame1.java
+++ FinalGame1.java
@@ -1,11 +1,27 @@
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
-public class FinalGame1{
-    public static void main(String[] args){
+import java.awt.event.*;
+
+public class FinalGame1 {
+    public static void main(String[] args) {
         JFrame frame = new JFrame("Portal 2d");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1080, 720);
+
+        JPanel gamePanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.BLACK);
+                g.fillRect(360, 540, 50, 50);
+            }
+        };
+
+        frame.add(gamePanel);
+
+        frame.setVisible(true);
     }
-
-
 }
+
+
